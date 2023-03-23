27170249647907ad2efa2b08005a17601fa96027
yinzecheng <1156910334@qq.com>
2023-03-23 10:09:54 +0800

    reformated data

--- change.py
+++ change.py
@@ -4,22 +4,24 @@ import os
 
 cc = opencc.OpenCC('t2s')
 
-with open('sishuwujing/mengzi.json',encoding='utf8') as f:
+with open('mengxue/zhuzijiaxun.json',encoding='utf8') as f:
     all_data=json.load(f)
     new_json=[]
     
-    for data in all_data:
-        new_dict={}
-        for k,v in data.items():
-            if k=='paragraphs':
-                new_dict['content']=[cc.convert(text) for text in v]  
-            else:
-                new_dict[k]=cc.convert(v)
-            
-        new_json.append(new_dict)
+    # for data in all_data:
+    new_dict={}
+    for k,v in all_data.items():
+        if k=='origin' or k =='author' or k=='spells':
+            pass
+        elif k=='paragraphs':
+            new_dict['content']=[cc.convert(text) for text in v]  
+        else:
+            new_dict[k]=cc.convert(v)
+        
+    new_json.append(new_dict)
 
 content=json.dumps(new_json,ensure_ascii=False,indent=2)
-with open('四书五经/data.json','w',encoding='utf8') as f:
+with open('蒙学/sanzijing.json','w',encoding='utf8') as f:
     # json.dump(content,f,ensure_ascii=False)
     f.write(content)
 
